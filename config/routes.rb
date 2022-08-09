2a27bc7ae664d25dfe9ab5c1fab4c4f840c8cdfe
+ <+>
2022-08-09 18:19:39 -0400

    completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
+
+  delete "/logout", to: "sessions#destroy"
+
+  post "/login", to: "sessions#create"
+
+  get "/me", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
